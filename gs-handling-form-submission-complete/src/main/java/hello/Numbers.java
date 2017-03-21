78a668b2e60dbdf1664f5d117550868fe7dfab43
ryan.kelly <ryan.kelly@LT00AE304.Oxford.local>
2017-03-21 14:25:35 -0400

    New Project

---
+++ gs-handling-form-submission-complete/src/main/java/hello/Numbers.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class Numbers {
+
+	private int num1;
+	private int num2;
+	private int sum;
+	
+	public int getNum1()
+	{
+		return num1;
+	}
+	
+	public void setNum1()
+	{
+		
+	}
+	
+}
